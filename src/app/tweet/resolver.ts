8e3a8551218d209d3ddf13eb110a29b3740e9d0c
Badri-Vishal <badrivishalpadhy@gmail.com>
2024-12-20 05:04:55 +0530

    tweets/posts+

--- src/app/tweet/resolver.ts
+++ src/app/tweet/resolver.ts
@@ -1,7 +1,28 @@
+import { connect } from "http2";
+import { prismaClient } from "../../client/db";
 import { GraphqlContext } from "../user/interfaces";
 
-const mutations = {
-  createTweet: (parent: any, { }: {}, ctx: GraphqlContext) => {
-
-  }
+interface CreateTweetPayload {
+  content: string;
+  imageURL?: string;
 }
+
+const mutations = {
+  createTweet: async (
+    parent: any,
+    { payload }: { payload: CreateTweetPayload },
+    ctx: GraphqlContext
+  ) => {
+    if (!ctx.user) throw new Error("You are not authenticated");
+    const tweet = await prismaClient.tweet.create({
+      data: {
+        content: payload.content,
+        imageURL: payload.imageURL,
+        author: { connect: { id: ctx.user.id } }
+      }
+    })
+    return tweet;
+  },
+};
+
+export const resolvers = { mutations }
